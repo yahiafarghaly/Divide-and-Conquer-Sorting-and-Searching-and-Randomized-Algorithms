3c513ed941e2af5ed81060b45ae817b3993d1b24
yahiafarghaly <yahiacommunication@yahoo.com>
2019-02-27 10:49:09 +0200

    fix a wrong behaviour due to self-loop

--- Randomized_min_cut/src/Randomized_min_cut.cpp
+++ Randomized_min_cut/src/Randomized_min_cut.cpp
@@ -39,19 +39,34 @@ int getRandomNumber(int start,int end)
 	return rand() % (end+1) + start; // (end + 1) to get an actually range between end and start.
 }
 
-#undef DEBUG
+//#define DEBUG
 
 int main() {
 
-	vertices_list n_list;
-	edges_list m_list;
+	vertices_list n_list,n_lst;
+	edges_list m_list,m_lst;
+	int v1,v2,min_cut = 100000; // final two nodes with min cuts
+	int old_min_cut = min_cut;
+	//read_graph_data("./src/input_graphs/sample_graph.txt",n_list,m_list);
+	//read_graph_data("./src/input_graphs/6.txt",n_list,m_list);
+	//read_graph_data("./src/input_graphs/self_loop.txt",n_list,m_list);
 	read_graph_data("./src/input_graphs/quiz_input.txt",n_list,m_list);
-
 	std::cout << "#n = " << n_list.size() << ", #m = " << m_list.size() << "\n";
+	std::cout << "-----------------------------------\n";
+	do
+	{
+		n_lst.clear();
+		m_lst.clear();
+		std::copy(n_list.begin(), n_list.end(), std::back_inserter(n_lst));
+		std::copy(m_list.begin(), m_list.end(), std::back_inserter(m_lst));
 
-	int v1,v2,min_cut; // final two nodes with min cuts
-	min_cut = RKarger(n_list,m_list,v1,v2);
-	cout << "Min Cut = " << min_cut << endl;
+		min_cut = RKarger(n_lst,m_lst,v1,v2);
+		if(min_cut < old_min_cut)
+		{
+			cout << "Min Cut = " << min_cut << endl;
+			old_min_cut = min_cut;
+		}
+	}while(min_cut >= 15);
 
 	return 0;
 }
@@ -92,8 +107,6 @@ void read_graph_data(string file_path,
 	{
 		for(unsigned int j = i + 1; j < m_list.size(); j++)
 		{
-			if(m_list[i] == m_list[j])
-				m_list.erase(m_list.begin() + j);
 			if(m_list[i].first == m_list[j].second && m_list[i].second == m_list[j].first)
 				m_list.erase(m_list.begin() + j);
 
@@ -124,7 +137,6 @@ int RKarger(vertices_list& n_list,
 	init_randomGenerator();
 
 	int edge_idx;
-	int self_loop_idx = 0;
 #ifdef DEBUG
 	unsigned long iter_cnt = 0;
 #endif
@@ -157,7 +169,6 @@ int RKarger(vertices_list& n_list,
 		printf("\n");
 #endif
 		// update all edges with the new connection.
-		self_loop_idx = 0;
 		for_each(m_list.begin(), m_list.end(),
 				[&]( pair<int,int>& e)
 				{
@@ -165,16 +176,27 @@ int RKarger(vertices_list& n_list,
 						e.first = v1;
 					if(e.second == v2)
 						e.second = v1;
-					if(e.first == e.second) // it becomes a self loop, so remove it.
-					{
-						m_list.erase(m_list.begin() + self_loop_idx);
+				});
+
+		// Remove self loops
+		for(auto edge = m_list.begin(); edge != m_list.end() ; ++edge)
+		{
+			if(edge->first == edge->second)
+			{
 #ifdef DEBUG
-						printf("----> Removing self-loop(%d,%d)\n",e.first,e.second);
+				printf("----> Removing self-loop(%d,%d)\n",edge->first,edge->second);
 #endif
-					}
-					++self_loop_idx;
-				}
-		);
+				m_list.erase(edge);
+				/*
+				 * Go back two positions of the deleted position, since
+				 * edge is incremented by one at the end of the loop.
+				 * so, it will miss the second element that was after
+				 * the deleted element.
+				 * */
+				edge = edge - 2;
+			}
+		}
+
 #ifdef DEBUG
 		printf("----> (%d) Edges Connections:\n",m_list.size());
 		for_each(m_list.begin(), m_list.end(),
