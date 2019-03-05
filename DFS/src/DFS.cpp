5a4f68ab1705d6f72bb653f10a1e4093f6d13139
yahiafarghaly <yahiacommunication@yahoo.com>
2019-03-05 13:49:11 +0200

    added Topological sort using DFS

--- DFS/src/DFS.cpp
+++ DFS/src/DFS.cpp
@@ -9,7 +9,13 @@
 #include <iostream>
 
 #include "DirectedGraph.h"
+#include <string>
+#include <fstream>
+#include <sstream>
 using namespace std;
+
+template<class T>
+void readGraphDataFromFile(DirectedGraph<T>& g,string file_path);
 
 int main() {
 
@@ -18,9 +24,69 @@ int main() {
 	g.addArc('s','w');
 	g.addArc('w','t');
 	g.addArc('v','t');
-	g.addSinkVertex('t');
+	g.addVertex('t');
 	g.printGraphAdjacenyList();
-	g.DFS('t');
+	//g.DFS('t');
 
+	auto sequence = g.topologicalSort_DFS();
+
+	for(auto v : sequence)
+		std::cout << v << " " ;
+	std::cout << "\n";
+
+	std::cout << "###############################\n";
+
+	DirectedGraph<int> g2;
+	readGraphDataFromFile(g2,"./src/directed_graph1.txt");
+	g2.printGraphAdjacenyList();
+
+	auto sequence2 = g2.topologicalSort_DFS();
+	/*
+	 * Can be either
+	 * 1 2 5 4 3 7 6
+	 * 1 2 5 4 7 3 6
+	 * */
+	for(auto v : sequence2)
+		std::cout << v << " " ;
+	std::cout << "\n";
 	return 0;
+}
+
+
+template<class T>
+void readGraphDataFromFile(DirectedGraph<T>& g,string file_path)
+{
+	std::fstream in_file(file_path, std::ios_base::in);
+
+	string line;
+	list<T> vertices;
+	while(getline(in_file,line))
+	{
+		if(line.size() > 0)
+		{
+			stringstream stream(line);
+			T vertexName;
+			vertices.clear();
+			while (1)
+			{
+				stream >> vertexName;
+				if (!stream)
+				{
+					g.addVertex(vertexName);
+					break;
+				}
+				vertices.push_back(vertexName);
+			}
+
+			auto s = vertices.begin();
+			auto v = s;
+			v++;
+
+			while(v != vertices.end())
+			{
+				g.addArc(*s,*v);
+				v++;
+			}
+		}
+	}
 }
--- DFS/src/DirectedGraph.h
+++ DFS/src/DirectedGraph.h
@@ -18,17 +18,20 @@ template <class T>
 class DirectedGraph {
 	std::map<T,std::list<T>> adjacencyList;
 	void __DFS(T const&, std::map<T,bool>&);
+	void __DFS_Topological(T const&, std::map<T,bool>&,
+			std::map<T,unsigned long>&, unsigned long&);
 public:
 	DirectedGraph();
 
 	void addArc(T const& v, T const& w);
-	void addSinkVertex(T const& v);
+	void addVertex(T const& v);
 	void printGraphAdjacenyList();
 	unsigned long verticesCount(void)
 	{
 		return this->adjacencyList.size();
 	}
 	void DFS(T const& s);
+	std::list<T> topologicalSort_DFS();
 
 	virtual ~DirectedGraph();
 };
@@ -42,7 +45,7 @@ void DirectedGraph<T>::addArc(T const& v, T const& w)
 
 
 template <class T>
-void DirectedGraph<T>::addSinkVertex(T const& v)
+void DirectedGraph<T>::addVertex(T const& v)
 {
 	this->adjacencyList[v];
 }
@@ -63,7 +66,7 @@ void DirectedGraph<T>::printGraphAdjacenyList(void)
 
 
 template <class T>
-void DirectedGraph<T>::__DFS(T const& v,std::map<T,bool>& v_discovery)
+void DirectedGraph<T>::__DFS(T const& v, std::map<T,bool>& v_discovery)
 {
 	auto edges = this->adjacencyList[v];
 	for(auto w : edges)
@@ -96,6 +99,63 @@ void DirectedGraph<T>::DFS(T const& s)
 
 }
 
+
+template <class T>
+void DirectedGraph<T>::__DFS_Topological(T const& v, std::map<T,bool>& v_discovery
+		,std::map<T,unsigned long>& f, unsigned long& current_label)
+{
+	auto edges = this->adjacencyList[v];
+	for(auto w : edges)
+	{
+		if(!v_discovery[w])
+		{
+			v_discovery[w] = true;
+			std::cout << "Vertex [ " << w << " ] is visited\n";
+			this->__DFS_Topological(w,v_discovery,f,current_label);
+		}
+	}
+	f[v] = current_label;
+	current_label -= 1;
+}
+
+template <class T>
+std::list<T> DirectedGraph<T>::topologicalSort_DFS()
+{
+	std::list<T> topological_sorted_list;
+	std::map<T,unsigned long> f;
+	std::map<T,bool> v_discovery;
+	unsigned long current_label = this->verticesCount();
+
+	for(auto v : this->adjacencyList)
+		v_discovery[v.first] = false;
+
+	for(auto v : this->adjacencyList)
+	{
+		if(!v_discovery[v.first])
+		{
+			v_discovery[v.first] = true;
+			std::cout << "Vertex [ " << v.first << " ] is visited\n";
+			this->__DFS_Topological(v.first,v_discovery,f,current_label);
+		}
+	}
+
+	// sort vertices labels.
+	current_label = this->verticesCount();
+	while(current_label)
+	{
+		for(auto v : this->adjacencyList)
+		{
+			if(f[v.first] == current_label)
+			{
+				topological_sorted_list.push_front(v.first);
+				--current_label;
+			}
+		}
+	}
+
+	return topological_sorted_list;
+
+}
 
 template <class T>
 DirectedGraph<T>::DirectedGraph () {
